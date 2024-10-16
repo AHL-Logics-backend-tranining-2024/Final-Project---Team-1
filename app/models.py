fd4699549c9ac9f7101f0d6baecd307fffe7900c
Omar <omarghandi209@gmail.com>
2024-10-16 21:11:34 +0300

    Fix models

--- app/models.py
+++ app/models.py
@@ -19,13 +19,6 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-class Status(Base):
-    __tablename__ = "statuses"
-
-    id = Column(uuid(as_uuid=True), primary_key=True, default=uuid, index=True)
-    name = Column(String(20), unique=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
 class Product(Base):
     __tablename__ = "products"
@@ -39,3 +32,53 @@ class Product(Base):
 
     def __repr__(self):
         return f"<Product(name={self.name}, price={self.price}, stock={self.stock}, is_available={self.is_available})>"
+
+        orders = relationship("Order", back_populates="user", cascade="all, delete-orphan")
+
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(uuid.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
+    user_id = Column(uuid.UUID(as_uuid=True), ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
+    status_id = Column(uuid.UUID(as_uuid=True), ForeignKey("order_status.id", ondelete="SET NULL"), nullable=True)
+    total_price = Column(Numeric(10, 2), nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), onupdate=datetime.now(timezone.utc))
+
+    user = relationship("User", back_populates="orders")  
+    status = relationship("OrderStatus", back_populates="orders")
+    products = relationship("OrderProduct", back_populates="order", cascade="all, delete-orphan")
+
+
+class OrderStatus(Base):
+    __tablename__ = "order_status"
+
+    id = Column(uuid.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), onupdate=datetime.now(timezone.utc))
+
+    orders = relationship("Order", back_populates="status")
+
+
+class OrderProduct(Base):
+    __tablename__ = "order_product"
+
+    id = Column(uuid.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
+    order_id = Column(uuid.UUID(as_uuid=True), ForeignKey("orders.id", ondelete="CASCADE"), nullable=False)
+    product_id = Column(uuid.UUID(as_uuid=True), ForeignKey("products.id", ondelete="SET NULL"), nullable=True)
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), onupdate=datetime.now(timezone.utc))
+
+    order = relationship("Order", back_populates="products")
+    
+class Status(Base):
+    __tablename__ = "statuses"
+
+    id = Column(uuid(as_uuid=True), primary_key=True, default=uuid, index=True)
+    name = Column(String(20), unique=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
